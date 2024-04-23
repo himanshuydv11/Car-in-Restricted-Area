53cfed6eaf1f2aa0f7c19584be3cd783621a3e4b
Himanshu <himanshuyadav09911@gmail.com>
2024-04-23 18:02:24 +0530

    change in main file

--- main.py
+++ main.py
@@ -33,51 +33,57 @@ classnames = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'trai
 
 while True:
     success, img = video.read()
-    results = model(img, stream=True)
 
-    # res = results[0].boxes.data
-    # px = pd.DataFrame(res).astype("float")
+    if success:
 
-    # for index, row in px.iterrows():
-    #     x1 = int(row[0])
-    #     y1 = int(row[1])
-    #     x2 = int(row[2])
-    #     y2 = int(row[3])
-    #     d = int(row[5])
-    #     c = classnames[d]
+        results = model(img, stream=True)
 
-    for r in results:
-        boxes = r.boxes
+        # res = results[0].boxes.data
+        # px = pd.DataFrame(res).astype("float")
 
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        # for index, row in px.iterrows():
+        #     x1 = int(row[0])
+        #     y1 = int(row[1])
+        #     x2 = int(row[2])
+        #     y2 = int(row[3])
+        #     d = int(row[5])
+        #     c = classnames[d]
 
-            cls = int(box.cls[0])
-            cls = classnames[cls]
+        for r in results:
+            boxes = r.boxes
 
-            if 'car' in cls:
-                ans1 = cv2.pointPolygonTest(np.array(area3, np.int32), ((x2,y2)), False)
-                ans2 = cv2.pointPolygonTest(np.array(area3, np.int32), ((x1,y2)), False)
-                ans3 = cv2.pointPolygonTest(np.array(area3, np.int32), ((x1,y1)), False)
-                ans4 = cv2.pointPolygonTest(np.array(area3, np.int32), ((x2,y1)), False)
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
-                if ans1 > 0 or ans2 > 0 or ans3 > 0 or ans4 > 0:
-                    # for playing note.wav file
-                    my_sound.play()
-                    cv2.rectangle(img, (x1,y1), (x2,y2), (0,0,255),1,1)
-                    cv2.putText(img, f'Car in restricted area zone', (x1,y1), cv2.FONT_HERSHEY_COMPLEX, (0.5),(255,255,255),1)
-                else:
-                    cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0),1,1)
-                    cv2.putText(img, str(cls), (x1,y1), cv2.FONT_HERSHEY_COMPLEX, (0.5),(255,255,255),1)
+                cls = int(box.cls[0])
+                cls = classnames[cls]
+
+                if 'car' in cls:
+                    ans1 = cv2.pointPolygonTest(np.array(area3, np.int32), ((x2,y2)), False)
+                    ans2 = cv2.pointPolygonTest(np.array(area3, np.int32), ((x1,y2)), False)
+                    ans3 = cv2.pointPolygonTest(np.array(area3, np.int32), ((x1,y1)), False)
+                    ans4 = cv2.pointPolygonTest(np.array(area3, np.int32), ((x2,y1)), False)
+
+                    if ans1 > 0 or ans2 > 0 or ans3 > 0 or ans4 > 0:
+                        # for playing note.wav file
+                        my_sound.play()
+                        cv2.rectangle(img, (x1,y1), (x2,y2), (0,0,255),1,1)
+                        cv2.putText(img, f'Car in restricted area zone', (x1,y1), cv2.FONT_HERSHEY_COMPLEX, (0.5),(255,255,255),1)
+                    else:
+                        cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0),1,1)
+                        cv2.putText(img, str(cls), (x1,y1), cv2.FONT_HERSHEY_COMPLEX, (0.5),(255,255,255),1)
     
-    cv2.polylines(img, [np.array(area3, np.int32)], True, (0,0,255),2)
+        cv2.polylines(img, [np.array(area3, np.int32)], True, (0,0,255),2)
 
-    cv2.imshow("Image", img)
+        cv2.imshow("Image", img)
 
-    k = cv2.waitKey(1)
+        k = cv2.waitKey(1)
 
-    if k == ord('q'):
+        if k == ord('q'):
+            break
+        
+    else:
         break
 
 video.release()
